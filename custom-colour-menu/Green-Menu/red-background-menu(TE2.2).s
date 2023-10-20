4e765ff250da50ad4c705effca144c964eb84e97
Joeatron <63060723+joeatron@users.noreply.github.com>
2023-10-20 23:26:18 +0100

    Added TE 2.2 version

---
+++ custom-colour-menu/Green-Menu/red-background-menu(TE2.2).s
@@ -0,0 +1,26 @@
+##################################################################
+# Address: 801de288
+# Codetype: Auto
+# Annotation: red menu (TE2.2)
+##################################################################
+
+#load run counter
+lis r12, 0x80E1
+ori r12, r12, 0x9CE0
+lbz r11, 0(r12)
+
+#compare for the run that contrains background
+cmpwi r11, 0x1
+beq end 
+
+#load green screen
+loadgreen:
+lis r11, 0xFF00
+ori r11, r11, 0x00FF
+stw r11, 0x00AC(r25)
+
+
+end:
+li r11, 0x1			#bump counter
+stb r11, 0(r12)
+lwz	r28, 0x00AC (r25)		#og line
